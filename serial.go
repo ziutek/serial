5dd852392c218a9c9cd094c97e8ca3f37953bdc0
Michał Derkacz <michal@Lnet.pl>
2012-06-27 11:07:17 +0200

    Small fixes in doc

--- serial.go
+++ serial.go
@@ -10,8 +10,7 @@ type Serial struct {
 }
 
 // Defaults:
-// 9600 8N1, soft/hard flow controll off
-// raw mode, vmin=1, vtime=0
+//  9600 8N1, soft/hard flow controll off, raw mode, vmin=1, vtime=0
 func Open(name string) (*Serial, error) {
 	f, err := os.OpenFile(
 		name,
@@ -82,16 +81,14 @@ func (s *Serial) SetMode(canon bool) error {
 }
 
 // Sets Read behavior for noncanonical mode.
-// vmin  - minimum number of characters for Read
-// vtime - timeout in deciseconds
-//
-// vmin == 0 && vtime == 0 : non-blocking Read
-//
-// vmin == 0 && vtime > 0  : Read returns buffered charcters or waits vtime
-//                           for new charcters
-// vmin > 0  && vtime > 0  : Read returns n >= vmin charcters or
-//                           0 < n < vmin if vtime expires after n-th char
-// vmin > 0  && vtime == 0 : Read returns at least vmin characters
+//  vmin  - minimum number of characters for Read,
+//  vtime - timeout in deciseconds,
+//  vmin == 0 && vtime == 0 : non-blocking Read,
+//  vmin == 0 && vtime > 0  : Read returns buffered charcters or waits
+//                            vtime for new charcters,
+//  vmin > 0  && vtime > 0  : Read returns n >= vmin charcters or
+//                            0 < n < vmin if vtime expires after n-th char
+//  vmin > 0  && vtime == 0 : Read returns at least vmin characters
 func (s *Serial) SetRawRead(vmin, vtime int) error {
 	return s.setRawRead(vmin, vtime)
 }
